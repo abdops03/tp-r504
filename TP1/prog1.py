2779994beaecbe20e4b6a8b5b1e81940b5879d48
abdoulaye <gaye.abdoulaye.pro@gmail.com>
2026-09-16 14:16:25 +0200

    maj

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -6,7 +6,7 @@ print("Appuyez sur CTRL+C pour quitter.")
 
 while True:
     
-    a = float(input("Entrez un nombre : "))
+    a = float(input("Entrez un nombre  : "))
     b = float(input("Entrez un nombre : "))
     res = f.puissance(a,b)
     print(res)
