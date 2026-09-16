352c9d9937d7a8bb5b71ba792ad93992fdccfb07
abdoulaye <gaye.abdoulaye.pro@gmail.com>
2026-09-16 14:15:35 +0200

    maj

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,6 @@
-
-
 def puissance (a,b):
+	if not type(a) is int or not type(b) is int:
+		raise TypeError("Only integers are allowed")
 	return a**b
 
 
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -6,7 +6,7 @@ print("Appuyez sur CTRL+C pour quitter.")
 
 while True:
     
-    a = int(input("Entrez un nombre : "))
-    b = int(input("Entrez un nombre : "))
+    a = float(input("Entrez un nombre : "))
+    b = float(input("Entrez un nombre : "))
     res = f.puissance(a,b)
-	print("res="+res)
+    print(res)
